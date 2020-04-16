72386f7f6a1740215bbd747e82f11fb6db6fc553
JeffFu <1093928135@qq.com>
2020-04-16 21:08:12 +0800

    wallpaper

--- UNIX/change_bg.sh
+++ UNIX/change_bg.sh
@@ -2,3 +2,7 @@
 # 以"作为cut的分隔符
 bgLink=`curl cn.bing.com | grep -e "link id=\"bgLink\"" | cut -f6 -d\"`
 wget "https://cn.bing.com$bgLink"
+
+# change wallpaper
+# filename = bgLink
+gsettings set org.gnome.desktop.background picture-uri "$PWD/$bgLink"
