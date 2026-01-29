b9f60251180f1889ea2f463817bfe42f584a6ccb
Jeff Fu <jefffu233@gmail.com>
2026-01-28 20:00:40 -0800

    Floyd: add comments, use better index

--- Templates/Floyd.py
+++ Templates/Floyd.py
@@ -5,9 +5,18 @@ def floyd(graph: List[List[int]]) -> List[List[int]]:
     """
     graph: adjacent matrix
     """
+    # dp[k][i][j]: min cost from i to j through 0, 1, ..., k
+    # dp[-1][i][j] = graph[i][j]
+    # dp[k][i][j] = min(dp[k-1][i][j], dp[k-1][i][k] + dp[k-1][k][j])
+    # ans: dp[n-1][i][j]
+
+    # note that dp[k-1][i][k] <= dp[k][i][k], and dp[k][i][k] <= dp[k-1][i][k]
+    # therefore dp[k][i][k] == dp[k-1][i][k]
+    # same for dp[k][k][j] == dp[k-1][k][j]
+    # so we can update graph inplace
     n = len(graph)
-    for k in range(1, n + 1):
+    for k in range(n):
         for i in range(n):
             for j in range(n):
-                graph[i][j] = min(graph[i][j], graph[i][k - 1] + graph[k - 1][j])
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
     return graph
